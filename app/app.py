7dc27ab4f21de108db3983360e5f1972227128ba
Logan Miklosovic <logmiklos@gmail.com>
2025-01-29 18:13:20 -0500

    add headers and configure app and database

---
+++ app/app.py
@@ -0,0 +1,16 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///data/skillsift.db")
+
+@app.after_request
+def after_request(response):
+    # Ensure responses aren't cached
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
