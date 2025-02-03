a4e9afe6947bdce7853b9c04660ea2a60781ea76
Logan Miklosovic <logmiklos@gmail.com>
2025-02-03 15:16:17 -0500

    refactor main function & import utility functions also create event listener for location button

--- app/static/main.js
+++ app/static/main.js
@@ -1,32 +1,18 @@
+import * as helpers from "./helpers.js";
+
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, handleError);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const latitude = position.coords.latitude;
+			const longitude = position.coords.longitude;
+			console.log(`Longitude: ${longitude} Latitude: ${latitude}`);
+			helpers.sendLocationToServer(latitude, longitude);
+		}, helpers.handleError);
+	} else {
+		console.log("Geolocation is not supported by this browser.");
+	}
 }
 
-function showPosition(position) {
-    const longitude = position.coords.longitude;
-    const latitude = position.coords.latitude;
-
-    console.log(latitude, longitude);
-}
-
-function handleError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            console.log("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            console.log("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            console.log("An unknown error occurred.");
-            break;
-    }
-}
-  
+document
+	.getElementById("location-button")
+	.addEventListener("click", getLocation);
