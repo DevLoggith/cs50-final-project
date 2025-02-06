d37cd4390c720dccad10d0bbc0d60a25a6906793
Logan Miklosovic <logmiklos@gmail.com>
2025-02-06 16:10:01 -0500

    create basis of 'scrape' route also make todo's for list and charts route pages

--- app/app.py
+++ app/app.py
@@ -16,12 +16,49 @@ def after_request(response):
 def index():
     return render_template("index.html")
 
+# TODO: create 'List' route
+# displays a list of all skills found and how many times they appear
 
-# TODO: create /save_location route in order to store a users location data -
-# longitude & latitude - gathered from the geolocation API
+# TODO: create 'Charts' route
+# page with different charts displaying frequency of tech keywords found
 
-# @app.route("/search", methods=["GET", "POST"])
-# def search():
+# TODO: create scrape route
+@app.route('/scrape', methods=['POST'])
+def scrape_jobs():
+    # receive JSON data from sendLocationToServer function
+    data = request.get_json()
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+
+    # get from data from manual input
+    job_title = request.form.get('job_title')
+    manual_location = request.form.get('location')
+
+    # determine which method of location input to use
+    if latitude and longitude:
+        coords = f"{latitude}, {longitude}" # TODO: what format to pass to 'geocode'
+        location = geocode(coords)
+    elif manual_location:
+        location = manual_location
+    else:
+        return jsonify({"error": "No location data provided"}), 400
+    
+    # run web scraper (scrapy)
+    keywords = scrape_job_postings(job_title, location, limit=100)
+    
+    # return results to the client for storage
+    return jsonify({
+        "location": location,
+        "keywords": keywords
+    })
+
+# def scrape_job_postings(lob_title, location, limit):
+    # placeholder for web scraping logic
+    # return ["keyword1", "keyword2", "keyword3"]
+
+# def geocode(location):
+    # placeholder for geocoding logic (convert to human readable address)
+    # return "Cleveland, OH", or "44107"
 
 if __name__ == "__main__":
     app.run(debug=True)
