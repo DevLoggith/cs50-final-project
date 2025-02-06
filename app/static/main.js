20d0a7a977e82f3c57cf4271eeda219fd6bc7f9b
Logan Miklosovic <logmiklos@gmail.com>
2025-02-06 14:48:47 -0500

    move helper functions to main.js delete helpers.js to simplify. refactor and abstract later if need be

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,3 @@
-import * as helpers from "./helpers.js";
-
 document
 	.getElementById("location-button")
 	.addEventListener("click", getLocation);
@@ -14,5 +12,39 @@ function getLocation() {
 		}, helpers.handleError);
 	} else {
 		console.log("Geolocation is not supported by this browser.");
+	}
+}
+
+
+function handleError(error) {
+	switch (error.code) {
+		case error.PERMISSION_DENIED:
+			console.log("User denied the request for Geolocation.");
+			break;
+		case error.POSITION_UNAVAILABLE:
+			console.log("Location information is unavailable.");
+			break;
+		case error.TIMEOUT:
+			console.log("The request to get user location timed out.");
+			break;
+		case error.UNKNOWN_ERROR:
+			console.log("An unknown error occurred.");
+			break;
+	}
+}
+
+async function sendLocationToServer(latitude, longitude) {
+	try {
+		const response = await fetch("/save_location", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ latitude: latitude, longitude: longitude }),
+		});
+		const data = await response.json();
+		console.log("location saved:", data);
+	} catch (error) {
+		console.log("Error: ", error);
 	}
 }
