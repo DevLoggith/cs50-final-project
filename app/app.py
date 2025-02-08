7a2ff5e0a34942d77ddeee0f5f0b07e67745d980
Logan Miklosovic <logmiklos@gmail.com>
2025-02-07 20:07:45 -0500

    remove logic for receiving data from js - remove geocode() definition. moved to front end - can access form data via request.form.get()

--- app/app.py
+++ app/app.py
@@ -25,29 +25,16 @@ def index():
 # TODO: create scrape route
 @app.route('/scrape', methods=['POST'])
 def scrape_jobs():
-    # receive JSON data from sendLocationToServer function
-    data = request.get_json()
-    latitude = data.get('latitude')
-    longitude = data.get('longitude')
-
     # get from data from manual input
     job_title = request.form.get('job_title')
-    manual_location = request.form.get('location')
-
-    # determine which method of location input to use
-    if latitude and longitude:
-        coords = f"{latitude}, {longitude}" # TODO: what format to pass to 'geocode'
-        location = geocode(coords)
-    elif manual_location:
-        location = manual_location
-    else:
-        return jsonify({"error": "No location data provided"}), 400
+    location = request.form.get('location')
     
     # run web scraper (scrapy)
     keywords = scrape_job_postings(job_title, location, limit=100)
     
     # return results to the client for storage
     return jsonify({
+        # other data needed? just keywords data needed?
         "location": location,
         "keywords": keywords
     })
@@ -55,10 +42,6 @@ def scrape_jobs():
 # def scrape_job_postings(lob_title, location, limit):
     # placeholder for web scraping logic
     # return ["keyword1", "keyword2", "keyword3"]
-
-# def geocode(location):
-    # placeholder for geocoding logic (convert to human readable address)
-    # return "Cleveland, OH", or "44107"
 
 if __name__ == "__main__":
     app.run(debug=True)
