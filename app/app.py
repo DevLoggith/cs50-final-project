22c821e08f1190e421bad5049fb9dab257bc0011
logan <92327020+DevLoggith@users.noreply.github.com>
2025-04-10 13:01:55 -0400

    passes data to list route & updates scrape limit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template, request, jsonify
+from flask import Flask, jsonify, redirect, render_template, request
 from scrape import scrape_job_descriptions
 from extract import extract_total_keywords
 
@@ -19,7 +19,7 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    # retrieve env variables and pass to font end
+    # retrieve env variables and pass to font end JS
     nominatim_user_agent = os.getenv("NOMINATIM_USER_AGENT")
     return render_template("index.html", nominatim_user_agent=nominatim_user_agent)
     # TODO: add form validation
@@ -45,10 +45,10 @@ def scrape_jobs():
     job_title = request.form.get("job_title")
     location = request.form.get("location")
     
-    job_descriptions = scrape_job_descriptions(job_title, location, limit=25)
+    job_descriptions = scrape_job_descriptions(job_title, location, limit=10)
     keywords_dict = extract_total_keywords(job_descriptions)
-    print(keywords_dict)
-    return keywords_dict
+    # convert dict to JSON via jsonify before storing it in sessionStorage
+    return redirect("list", keywords_dict=keywords_dict)
 
 
 if __name__ == "__main__":
