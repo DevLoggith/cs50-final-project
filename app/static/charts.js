acc632a9a970b6531b0717bcd8c4f546fd0b95ca
logan <92327020+DevLoggith@users.noreply.github.com>
2025-05-19 13:10:54 -0400

    adds proper padding to bottom of footer
    also updates background color (last time, i swear)

--- app/static/charts.js
+++ app/static/charts.js
@@ -85,7 +85,7 @@ function drawColumnChart(screenWidth) {
 			duration: 500,
 			easing: "out",
 		},
-		backgroundColor: "#f7f5f8"
+		backgroundColor: "#f0f5f6"
 	};
 
 	if (screenWidth < 768) {
@@ -137,7 +137,7 @@ function drawPieChart(screenWidth) {
 		title: "Tech Skills Donut Chart",
 		pieHole: 0.4,
 		sliceVisibilityThreshold: 1 / 64,
-		backgroundColor: "#f7f5f8"
+		backgroundColor: "#f0f5f6"
 	};
 
 	if (screenWidth < 992) {
