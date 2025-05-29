a870df002bfc48350840e965f828eff98b1c3982
logan <92327020+DevLoggith@users.noreply.github.com>
2025-05-29 11:31:15 -0400

    adds specific user agent to Chrome options
    problem:
    program was timing out during the "Waiting for results..." phase
    
    fixes tried:
    - increasing the wait times
    - using implicit waits
    - multiple different Chrome options
    - implicitly setting a window size for the browser
    - upgrading undetected-chromedriver & Chrome again
    - clearing Chrome user data, undetected-chromedriver cache, & Chrome cache directory
    
    fixes worked:
    assigning a specific user agent

--- app/scrape.py
+++ app/scrape.py
@@ -31,6 +31,7 @@ def initialize_browser():
     options.add_argument("--disable-gpu")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36')
     
     logger.info("Initializing Chrome browser...")
     return uc.Chrome(options=options)
